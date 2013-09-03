ae95f6b869d4b16a00ea0a09089b90559cf926b0
jakemcknight <jakemc321@tamu.edu>
2013-09-03 09:59:55 -0500

    Possible Skeleton?
    Just had a little free time so I thought I'd write the raw basics of the class. - JM

--- Database.h
+++ Database.h
@@ -1,3 +1,22 @@
-class Header{
-  
+class Database{
+  Private:
+    List<vector<vector<string>>> TABLE_LIST;
+  Public:
+    //Query Functions             Just defining base functions return types and arguments will most likely not be correct -JM 
+    void select();
+    void project();
+    void rename();
+    void set_union();
+    void set_difference();
+    void cross_product();
+    
+    //Command Functions
+    void insert();
+    void close();
+    void write();
+    void show();
+    void create();
+    void update();
+    void insert();
+    void delete();
 }
