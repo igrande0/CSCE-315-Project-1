798387e06e75a256a3c33ee9df409693c854c345
Isaac Anthony Grande <igrande0@sun.cs.tamu.edu>
2013-09-09 17:14:03 -0500

    nearly finished DBTest file

--- DBTest.cpp
+++ DBTest.cpp
@@ -1,5 +1,6 @@
 #include "Database.h"
 #include <string>
+#include <iostream>
 
 int main() {
 	Database db;
@@ -46,26 +47,86 @@ int main() {
 	tuple4.push_back("2004-06-13");
 	tuple4.push_back("89.38");
 
+	cout << "TEST CREATE(), INSERT_TUPLE(), SHOW(): create and show a table\n";
 	db.create(title, attributes, attribute_types, keys);
 	db.insert_tuple(title, tuple1);
 	db.insert_tuple(title, tuple2);
 	db.insert_tuple(title, tuple3);
 	db.insert_tuple(title, tuple4);
-
 	db.show(title);
+	cout << "\n\n";
+	
+	vector<string> tuple5;
+	vector<string> tuple6;
+	vector<string> tuple7;
+	vector<string> tuple8;
+	
+	tuple5.push_back("Jake");
+	tuple5.push_back("57");
+	tuple5.push_back("1991-10-18");
+	tuple5.push_back("54.32");
+
+	tuple6.push_back("Joe");
+	tuple6.push_back("10");
+	tuple6.push_back("2015-01-05");
+	tuple6.push_back("85.3");
+
+	tuple7.push_back("Charlotte");
+	tuple7.push_back("3");
+	tuple7.push_back("2033-08-08");
+	tuple7.push_back("65.54");
+
+	tuple8.push_back("Doug");
+	tuple8.push_back("4");
+	tuple8.push_back("2004-06-13");
+	tuple8.push_back("89.38");
+	db.create("table2", attributes, attribute_types, keys);
+	db.insert_tuple("table2", tuple5);
+	db.insert_tuple("table2", tuple6);
+	db.insert_tuple("table2", tuple7);
+	db.insert_tuple("table2", tuple8);
+	cout<<"\n\n TABLE 2:\n";
+	db.show("table2");
+	
+	cout << "TEST REMOVE(): remove row 4\n";
 	db.remove(title, 4);
 	db.show(title);
-	vector<string> temp_at;
-	temp_at.push_back("Attribute 2");
-	vector<string> temp_new;
-	temp_new.push_back("55");
-	db.update(title, temp_at, temp_new, 3);
+	cout << "\n\n";
+	
+	cout << "TEST UPDATE(): change Attribute 2 on row 3 to '55'\n";
+	vector<string> attribute;
+	attribute.push_back("Attribute 2");
+	vector<string> data;
+	data.push_back("55");
+	db.update(title, attribute, data, 3);
 	db.show(title);
+	cout << "\n\n";
+	
+	cout << "TEST SELECT(): select row 3 and row 5\n";
 	db.select("view1", title, 3);
 	db.select("view1", title, 5);
-	db.show("view1");              //dumps core? couldnt figure out gbd - JM
+	db.show("view1");
+	cout << "\n\n";
+		
+	cout << "TEST INSERT_VIEW(): insert view1 into Table\n";
+	db.insert_view(title, "view1");
+	db.show(title);
+	cout << "\n\n";
 	
-
-
-
+	cout << "TEST PROJECT(): project the Attribute 2 column\n";
+	db.project("view2", title, attribute);
+	db.show("view2");
+	
+	cout<< "Test RENAME(): \n";
+	attributes[0] = "Attribute 5";
+	attributes[1] = "Attribute 6";
+	attributes[2] = "Attribute 7";
+	attributes[3] = "Attribute 8";
+	db.rename("view3", title, attributes);
+	db.show("view3");
+	cout << "\n\n";
+	
+	
+	
+	
 }
