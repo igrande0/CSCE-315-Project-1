e212b0714a3209025dfd0df0bdf3651ee226398f
Jake Tyler Mcknight <jtm2183@sun.cs.tamu.edu>
2013-09-25 17:03:47 -0500

    Added functions to application

--- Application.cpp
+++ Application.cpp
@@ -1,6 +1,29 @@
 #include "Application.h"
 
-int main(){
-  
-  return 0;
+//bank - (name, routing#, address)
+//account - (account#, type, balance)
+//person - (name, address, birthdate, phone#)
+
+void Application::add_bank(){
+
+}
+
+void Application::remove_bank(){
+
+}
+
+void Application::add_account(){
+
+}
+
+void Application::remove_account(){
+
+}
+
+void Applcation::add_person(){
+
+}
+
+void remove_person(){
+
 }
--- Application.h
+++ Application.h
@@ -1,5 +1,15 @@
+#include "Parser.h"
+
+using namespace std;
+
 class Application{
-  private:
-    Parser parser;
-  
+	private:
+		Parser parser;
+	public:
+		void add_bank();
+		void remove_bank();
+		void add_account();
+		void remove_account();
+		void add_person();
+		void remove_person();
 };
