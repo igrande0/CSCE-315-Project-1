3ee6e2c819f613e31ec313e90be0040699633525
Isaac Anthony Grande <igrande0@gmail.com>
2013-09-29 15:25:06 -0500

    consolidated add functions; some other minor changes

--- Application.cpp
+++ Application.cpp
@@ -5,22 +5,15 @@ Application::Application() {
 		"bank name, bank routing number",
 		"account number, account type",
 		"name, birthdate",
-		"person's name, bank name",
-		"person's name, account number",
+		"client's name, bank name",
+		"client's name, account number",
 		"bank name, account number"};	
-	vector<vector<string> > attribute_types {
-		{"VARCHAR(255)", "INTEGER", "VARCHAR(255)"},
-		{"INTEGER", "VARCHAR(255)", "INTEGER"},
-		{"VARCHAR(255)", "VARCHAR(255)", "VARCHAR(255)", "INTEGER"},
-		{"VARCHAR(255)", "VARCHAR(255)"},
-		{"VARCHAR(255)", "INTEGER"},
-		{"VARCHAR(255)", "INTEGER"}};
 
 	// either open all tables or create all tables and save to disk
 	for(unsigned int i = 0; i < tables.size(); ++i)
 		if(!parser.execute("OPEN " + tables[i] + ";")) {
 			// construct CREATE TABLE command
-			string create = "CREATE TABLE " + tables[i] + "(";
+			string create = "CREATE TABLE " + tables[i] + " (";
 			for(unsigned int j = 0; j < attributes[i].size(); ++j){
 				create += attributes[i][j] + " " + attribute_types[i][j];
 				if(j != attributes[i].size()-1)
@@ -44,14 +37,14 @@ Application::~Application() {
 
 #define BANK 1
 #define ACCOUNT 2
-#define PERSON 3
+#define client 3
 
-int Application::main_menu(){
+/*int Application::main_menu(){
 	cout<<"Welcome to the Bank Databasing System!"<<endl;
 	cout<<"Please enter the integer corresponding to the entity you would like to add, change, ect.:"<<endl;
 	cout<<"Bank:\t1"<<endl;
 	cout<<"Account:\t2"<<endl;
-	cout<<"Person:\t3"<<endl;
+	cout<<"client:\t3"<<endl;
 	int MENU;
 	cin>>MENU;
 }
@@ -72,8 +65,8 @@ void Application::second_menu(int type){
 			case ACCOUNT:
 				add_account();
 				break;
-			case PERSON:
-				add_person();
+			case client:
+				add_client();
 				break;
 		}
 		break;
@@ -85,8 +78,8 @@ void Application::second_menu(int type){
 			case ACCOUNT:
 				remove_account();
 				break;
-			case PERSON:
-				remove_person();
+			case client:
+				remove_client();
 				break;
 		}
 		break;
@@ -98,130 +91,129 @@ void Application::second_menu(int type){
 			case ACCOUNT:
 				update_account();
 				break;
-			case PERSON:
-				update_person();
+			case client:
+				update_client();
 				break;
 		}
 		break;
 	}
-}
+}*/
 
-/*
 void Application::main_menu() {
-	int input;
-	display_main_menu();
-	cin >> input;
-	if (input == 1)
-	{
-		//call update
+	while(true) {
+		char choice;
+		display_main_menu();
+		cin >> choice;
+
+		switch(choice) {
+		case '1': case '2': case '3':
+		case '4': case '5': case '6':
+			table_menu(tables[choice-1]);
+			break;
+		case 'q':
+			return;
+			break;
+		default:
+			cout << "Invalid option. Please enter another option.\n";
+			break;
+		}
+		// add QUERIES somehow
 	}
-	else if (input == 2)
-	{
-		//call update
-	}
-	else if (input == 3)
-	{
-		//call update
-	}
-	else if (input == 4)
-	{
-		//call update
-	}
-	else if (input == 5)
-	{
-		//call update
-	}
-	else if (input == 6)
-	{
-		//call update
-	}
-	// LOOP
-	// display menu
-	// take in valid option #
-	// call table_menu(tables[i-1])
 }
 
 void Application::table_menu(string table) {
-	
-	// LOOP
-	// display menu
-	// take in valid option #
-	// call add(), remove(), or update() with correct tables[i] and corresponding attributes[i]
+	while(true) {
+		char choice;
+		display_table_menu(table);
+		cin >> choice;
+
+		switch(choice) {
+		case '1':
+			add(table, attributes[choice-1], attribute_types[choice-1]);
+			break;
+		case '2':
+			remove(table, attributes[choice-1], attribute_types[choice-1]);
+			break;
+		case '3':
+			update(table, attributes[choice-1], attribute_types[choice-1]);
+			break;
+		case 'q':
+			return;
+			break;
+		default:
+			cout << "Invalid option. Please enter another option.\n";
+			break;
+		}
+	}
 }
 
 void Application::display_main_menu() {
 	// CHANGE THESE -  maybe? I think they sound stupid - Isaac
 	cout << "What would you like to do? Enter the number of your choice.\n";
+	cout << "Enter 'q' at any time to exit this menu.\n";
 	cout << "(1) modify the bank list\n";
-	cout << "(2) modify the person list\n";
+	cout << "(2) modify the client list\n";
 	cout << "(3) modify the account list\n";
-	cout << "(4) modify the enrollment list\n";
-	cout << "(5) modify the authorization list\n";
+	cout << "(4) modify the bank enrollment list\n";
+	cout << "(5) modify the account authorization list\n";
 	cout << "(6) modify the account host list\n";
 	// ADD queries somehow?
 }
 
 void Application::display_table_menu(string table) {
-	// CHANGE - these won't make sense and will need to be fixed somehow
-	int choice; //not sure if this is the best way, but it's all I know
-	string tablename;
-	cout << "(1) Add a table\n";
-	cout << "(2) Remove a table\n";
-	cout << "(3) Update a table\n";
-	cout << "Indicate what you would like to do:\n";
-	cin >> choice;
-	if (choice == 1)
-	{
-		cout << "Please input the table name:\n";
-		cin >> tablename;
-		add(tablename, attributes);
-	}
-	else if (choice == 2)
-	{
-		cout << "Please input the table name:\n";
-		cin >> tablename;
-		remove(tablename, attributes);
-	}
-	else if (choice == 3)
-	{
-		cout << "Please input the table name:\n";
-		update(table, attributes);
-	}
-	else
-	{
-		throw runtime_error("Invalid Input");
-	}
-	
-	/*cout << "How would you like to modify the" << table << " list? Enter the number of your choice.\n";
+	cout << "How would you like to modify the " << table << " list? Enter the number of your choice.\n";
+	cout << "Enter 'q' at any time to exit this menu.\n";
 	cout << "(1) add a " << table << "\n";
-	cout << "(2) remove a" << table << "\n";
-	cout << "(3) update a" << table << "\n";
+	cout << "(2) remove a " << table << "\n";
+	cout << "(3) update a " << table << "\n";
 }
-*/
-/*
-void Application::add(string table, vector<string> attributes){
+
+
+void Application::add(string table, vector<string> attributes, vector<string> attribute_types){
+	vector<string> user_attributes(attributes.size());
+	
+	// collect attributes from user
+	for(unsigned int i = 0; i < attributes.size(); ++i) {
+		cout << "Enter the " << table << " " << attributes[i] << ": ";
+		cin >> user_attributes[i];
+	}
+
+	// construct INSERT INTO command
+	string command = "INSERT INTO " + table + " VALUES FROM (";
+	for(unsigned int i = 0; i < attributes.size(); ++i) {
+		if(attribute_types[i] == "INTEGER")
+			command += user_attributes[i];
+		else
+			command += "\"" + user_attributes[i] + "\"";
+		if(i != attributes.size()-1)
+			command += ",";
+	}
+	command += ");";
+
+	parser.execute(command);
+	parser.execute("WRITE " + table + ";");
 	// EXAMPLE: INSERT INTO baseball_players VALUES FROM ("Alexander", "Smith", "Pirates", 2, 150000);
 	// ask for every attribute
 	// construct/call INSERT INTO
 	// WRITE
 }
 
-void Application::remove(string table, vector<string> attributes){
+void Application::remove(string table, vector<string> attributes, vector<string> attribute_types){
 	// EXAMPLE: DELETE FROM dots WHERE (y1 <= 0);
 	// loop to ask what conditions
 	// construct/call DELETE FROM
 	// WRITE
 }
 
-void Application::update(string table, vector<string> attributes){
+void Application::update(string table, vector<string> attributes, vector<string> attribute_types){
 	// EXAMPLE: UPDATE dots SET x1 = 0 WHERE x1 < 0;
 	// loop to ask what conditions
 	// loop to ask what to change
 	// construct/call UPDATE
 	// WRITE
 }
-*/
 
+/*
 void Application::add_bank(){
 	string BANK_NAME;
 	string ROUTING_NUMBER;
@@ -283,34 +275,34 @@ void Application::remove_account(){
 	parser.execute("WRITE account;");
 }
 
-void Applcation::add_person(){
+void Applcation::add_client(){
 	string NAME;
 	string ADDRESS;
 	string BIRTH_DATE;
 	string PHONE_NUMBER;
-	cout<<"Enter new person's first and last name: ";
+	cout<<"Enter new client's first and last name: ";
 	cin>>NAME;
-	cout<<endl<<"Enter the person's address: ";
+	cout<<endl<<"Enter the client's address: ";
 	cin>>ADDRESS;
-	cout<<endl<<"Enter the person's birth date: ";
+	cout<<endl<<"Enter the client's birth date: ";
 	cin>>BIRTH_DATE;
-	cout<<endl<<"Enter the person's phone number (without dashes and spaces): ";
+	cout<<endl<<"Enter the client's phone number (without dashes and spaces): ";
 	cin>>PHONE_NUMBER;
 	cout<<endl;
 	
-	parser.execute("INSERT INTO person VALUES (\"" + NAME + "\", \"" + ADDRESS + "\", \"" + BIRTH_DATE + "\", " + PHONE_NUMBER + ");");
-	parser.execute("WRITE person;");
+	parser.execute("INSERT INTO client VALUES (\"" + NAME + "\", \"" + ADDRESS + "\", \"" + BIRTH_DATE + "\", " + PHONE_NUMBER + ");");
+	parser.execute("WRITE client;");
 }
 
-void remove_person(){
+void remove_client(){
 	string NAME;
 	string BIRTH_DATE;
-	cout<<"Enter the person's name you would like to remove: "
+	cout<<"Enter the client's name you would like to remove: "
 	cin<<NAME;
-	cout<<endl<<"Enter the person's birth date that you would like to remove: ";
+	cout<<endl<<"Enter the client's birth date that you would like to remove: ";
 	cin>>BIRTH_DATE;
 	
-	parser.execute("DELETE FROM person WHERE name=\"" + NAME + "\" AND birth_date=\"" + BIRTH_DATE + "\";");
-	parser.execute("WRITE person;");
-}
+	parser.execute("DELETE FROM client WHERE name=\"" + NAME + "\" AND birth_date=\"" + BIRTH_DATE + "\";");
+	parser.execute("WRITE client;");
+}*/
 
--- Application.h
+++ Application.h
@@ -12,36 +12,51 @@ class Application{
 	private:
 		Parser parser;
 
-		vector<string> tables {"bank", "account", "person", "enrollment", "authorization", "host"};
+		vector<string> tables {"bank", "account", "client", "enrollment", "authorization", "host"};
 
-		vector<string> bank_attributes {"bank name", "bank routing number", "bank address"};
-		vector<string> account_attributes {"account number", "account type", "account balance"};
-		vector<string> person_attributes {"name", "address", "birthdate", "phone number"};
-		vector<string> bank_enrollment {"person's name", "bank name"};
-		vector<string> account_authorization{"person's name", "account number"};
+		vector<string> bank_attributes {"name", "number", "address"};
+		vector<string> account_attributes {"number", "type", "balance"};
+		vector<string> client_attributes {"name", "address", "birthdate", "phone number"};
+		vector<string> bank_enrollment {"client name", "bank name"};
+		vector<string> account_authorization{"client name", "account number"};
 		vector<string> account_host{"bank name", "account number"};
 
-		vector<vector<string> > attributes {bank_attributes, account_attributes, person_attributes,
+		vector<string> keys {
+			"name, routing number",
+			"number, type",
+			"name, birthdate",
+			"client name, bank name",
+			"client name, account number",
+			"bank name, account number"};	
+
+		vector<vector<string> > attributes {bank_attributes, account_attributes, client_attributes,
 			bank_enrollment, account_authorization, account_host};
+
+		vector<vector<string> > attribute_types {
+			{"VARCHAR(255)", "INTEGER", "VARCHAR(255)"},
+			{"INTEGER", "VARCHAR(255)", "INTEGER"},
+			{"VARCHAR(255)", "VARCHAR(255)", "VARCHAR(255)", "INTEGER"},
+			{"VARCHAR(255)", "VARCHAR(255)"},
+			{"VARCHAR(255)", "INTEGER"},
+			{"VARCHAR(255)", "INTEGER"}};
 
 		void display_main_menu();
 		void display_table_menu(string table);
 		void table_menu(string table);
 		
-		int main_menu();
+		/*int main_menu();
 		int second_menu(int type);
 		void add_bank();
 		void remove_bank();
 		void update_bank();
 		void add_account();
 		void remove_account();
-		void add_person();
-		void remove_person();
+		void add_client();
+		void remove_client();*/
 
-		//void add(string table, vector<string> attributes);
-		//void remove(string table, vector<string> attributes);
-		//void update(string table, vector<string> attributes);
-
+		void add(string table, vector<string> attributes, vector<string> attribute_types);
+		void remove(string table, vector<string> attributes, vector<string> attribute_types);
+		void update(string table, vector<string> attributes, vector<string> attribute_types);
 
 	public:
 		Application();
